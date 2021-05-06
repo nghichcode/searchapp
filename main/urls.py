082e806f93845972e27a719d28c00bcd2321b2bd
nghichcode <nghichcode@gmail.com>
2021-05-06 20:15:58 +0700

    Init Stage - convert to express

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from django.views.static import serve
-from django.conf import settings
-from . import views
-
-app_name = 'home'
-urlpatterns = [
-    path('', serve, {'path': 'index.html', 'document_root': settings.FRONTEND_ROOT}),
-]
