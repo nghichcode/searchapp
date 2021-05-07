fe7f173619ed400a9a53a13b02f49ea1ed06b6ef
nghichcode <nghichcode@gmail.com>
2021-05-07 22:13:28 +0700

    Init Stage - final s1.0

--- app/search.js
+++ app/search.js
@@ -23,21 +23,21 @@ const SEARCH_TYPE = ConstantMap({
   PAGE: {label: 'Pages', placeholder: 'type:page ', val: 2}
 });
 
-const pages = [
-  {name: 'Demo Site', updateAt: '1620382169646'},
-  {name: 'Java', updateAt: '1620295769646'},
-  {name: 'Javascript', updateAt: '1620209369646'},
-  {name: '.NET', updateAt: '1620122969646'},
-  {name: 'Begin Ruby', updateAt: '1620036569646'},
-  {name: 'Begin APS', updateAt: '1619950169646'},
-];
 const sites = [
-  {name: 'Page A', updateAt: '1620382169646'},
-  {name: 'Login', updateAt: '1620295769646'},
-  {name: 'Register', updateAt: '1620209369646'},
-  {name: 'Home', updateAt: '1620122969646'},
-  {name: 'Tutorial', updateAt: '1620036569646'},
-  {name: 'Learn', updateAt: '1619950169646'},
+  {name: 'Demo Site', updateAt: '1620382169646', ownerId: 1, starred: true, trash: false},
+  {name: 'Java', updateAt: '1620295769646', ownerId: 2, starred: true, trash: false},
+  {name: 'Javascript', updateAt: '1620209369646', ownerId: 3, starred: false, trash: true},
+  {name: '.NET', updateAt: '1620122969646', ownerId: 4, starred: true, trash: false},
+  {name: 'Begin Ruby', updateAt: '1620036569646', ownerId: 1, starred: false, trash: true},
+  {name: 'Begin APS', updateAt: '1619950169646', ownerId: 5, starred: true, trash: false},
+];
+const pages = [
+  {name: 'Page A', updateAt: '1620382169646', ownerId: 1, starred: true, trash: true},
+  {name: 'Login', updateAt: '1620295769646', ownerId: 2, starred: false, trash: false},
+  {name: 'Register', updateAt: '1620209369646', ownerId: 3, starred: true, trash: false},
+  {name: 'Home', updateAt: '1620122969646', ownerId: 4, starred: true, trash: false},
+  {name: 'Tutorial', updateAt: '1620036569646', ownerId: 1, starred: true, trash: false},
+  {name: 'Learn', updateAt: '1619950169646', ownerId: 5, starred: true, trash: false},
 ];
 
 function search_pages(req, res) {
